1b184f04882c389058995b960c5c246a9e7d4a0a
wlandaurop <wlandaurop@gmail.com>
2022-08-20 11:02:09 +0200

    first commit

---
+++ ByeWorld.java
@@ -0,0 +1,11 @@
+package com.company;
+/*
+Creammos la clase ByeWorld
+para empezar.
+ */
+public class ByeWorld {
+    public static void main(String[] args) {
+        System.out.println("Adios desde clase ByeWorld"); //imprimimos despedida
+
+    }
+}
---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class HelloWorld {
+    /** Enumeramos documentacion
+     * Primero
+     * Segundo
+     *
+     * @param args
+     *
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola desde clase HelloWorld");
+
+    }
+}
